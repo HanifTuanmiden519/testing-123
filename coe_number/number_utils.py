4e3b31e31aefcc3307204f961bd378228ad8e756
Hanif tuanmiden <hanif19001@gmail.com>
2025-03-09 00:14:36 +0700

    Add FizzBuzz

--- coe_number/number_utils.py
+++ coe_number/number_utils.py
@@ -1,6 +1,9 @@
-def is_prime_list(numbers):
-    for num in numbers:
-        for n in range(2, num):
-            if num % n == 0:
-                return False
-    return True
+def fizzbuzz(x):
+    if x % 3 == 0 and x % 5 == 0:
+        return "FizzBuzz"
+    elif x % 3 == 0:
+        return "Fizz"
+    elif x % 5 == 0:
+        return "Buzz"
+    else:
+        return str(x)
--- tests/test_number_utils.py
+++ tests/test_number_utils.py
@@ -1,31 +1,22 @@
-from coe_number.number_utils import is_prime_list
+from coe_number.number_utils import fizzbuzz
 import unittest
 
-class PrimeListTest(unittest.TestCase):
-    def test_give_1_2_3_is_prime(self):
-        prime_list = [1, 2, 3]
-        is_prime = is_prime_list(prime_list)
-        self.assertTrue(is_prime)
+class FizzBuzzTest(unittest.TestCase):
+    def test_give_3_should_return_fizz(self):
+        self.assertEqual(fizzbuzz(3), "Fizz")
 
-    def test_give_4_should_not_be_prime(self):
-        prime_list = [4]
-        is_prime = is_prime_list(prime_list)
-        self.assertFalse(is_prime)
+    def test_give_5_should_return_buzz(self):
+        self.assertEqual(fizzbuzz(5), "Buzz")
 
-    def test_give_5_7_11_should_be_prime(self):
-        prime_list = [5, 7, 11]
-        is_prime = is_prime_list(prime_list)
-        self.assertTrue(is_prime)
+    def test_give_15_should_return_fizzbuzz(self):
+        self.assertEqual(fizzbuzz(15), "FizzBuzz")
 
-    def test_give_6_8_9_should_not_be_prime(self):
-        prime_list = [6, 8, 9]
-        is_prime = is_prime_list(prime_list)
-        self.assertFalse(is_prime)
+    def test_give_7_should_return_7(self):
+        self.assertEqual(fizzbuzz(7), "7")
 
-    def test_give_2_should_be_prime(self):
-        prime_list = [2]
-        is_prime = is_prime_list(prime_list)
-        self.assertTrue(is_prime)
+    def test_give_30_should_return_fizzbuzz(self):
+        self.assertEqual(fizzbuzz(30), "FizzBuzz")
 
 if __name__ == '__main__':
     unittest.main()
+
